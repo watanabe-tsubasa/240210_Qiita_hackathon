0d3b7dc0d7000c0b7cb93aeab237f04c3625d6f7
watanabe-tsubasa <t.watanabe423@gmail.com>
2024-02-11 13:38:37 +0900

    upd

--- test.ts
+++
@@ -1,9 +0,0 @@
-const main = async () => {
-  const res = await fetch('https://script.google.com/macros/s/AKfycbx82SX2k58XRPYDC5vrp7Bm7PgvEfwjuI-PN6EKs56wIy2TvSwtCO14T1UZSYVmo-Ag/exec?user_id=U1111111111&type=point', {
-    method :'GET'
-  });
-  const data = await res.json()
-  console.log(data);
-}
-
-main();
